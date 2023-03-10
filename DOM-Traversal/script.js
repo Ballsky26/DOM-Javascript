f007b8f4c3a55ab2ce4673845b02d6cf3153d28e
Ballsky22 <ballskyteam22@gmail.com>
2023-03-10 21:59:38 +0700

    DOM Traversal Method Prevent Default Javascript

--- DOM-Traversal/script.js
+++ DOM-Traversal/script.js
@@ -11,7 +11,7 @@ const close = document.querySelectorAll(".close");
 
 // for (let i = 0; i < close.length; i++) {
 //   close[i].addEventListener("click", function (e) {
-//     // close[i].parentElement.style.display = "none";
+//      close[i].parentElement.style.display = "none";
 //     e.target.parentElement.style.display = "none";
 //   });
 // }
@@ -19,9 +19,10 @@ const close = document.querySelectorAll(".close");
 close.forEach(function (el) {
   el.addEventListener("click", function (e) {
     e.target.parentElement.style.display = "none";
+    e.preventDefault();
   });
 });
 
-const nama = document.querySelector(".nama");
+// const nama = document.querySelector(".nama");
 // console.log(nama.parentElement.parentElement);
-console.log(nama.previousElementSibling);
+// console.log(nama.nextElementSibling.nextElementSibling);
